8dcb384d80c907428572bba841b618998116c5be
neox2891 <n.estrada2891@gmail.com>
2018-11-09 14:08:03 -0500

    MONGO_URI

--- src/server/server.js
+++ src/server/server.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
     );
 });
 
-mongoose.connect('mongodb://csa:Juniortupapa.1@ds123603.mlab.com:23603/csa_db', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
 
     if (err) {
         throw err;
